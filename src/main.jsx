0e0611b6df74cb166662ca1540d21392b4d97596
apdy <arakazaprincedestinyvan@gmail.com>
2026-08-28 14:05:09 +0200

    try to fix display programe and publication

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./hooks/useAuth.jsx";
 import "./index.css";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*",
+      element: (
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      ),
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      {/* AuthProvider wraps EVERYTHING so any page can ask "is someone logged in?" */}
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
